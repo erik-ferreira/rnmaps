35afd71ebb070a698cf97e084a4f54c2fea4b720
erik-ferreira <erikdesouzagf@gmail.com>
2023-05-04 13:38:25 -0300

    get position and set marker in map; observe when position change

--- App.tsx
+++ App.tsx
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker } from "react-native-maps";
+import { View, Text } from "react-native";
+import { useState, useEffect } from "react";
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+  LocationObject,
+  watchPositionAsync,
+  LocationAccuracy,
+} from "expo-location";
+
+import { styles } from "./styles";
 
 export default function App() {
+  const [location, setLocation] = useState<LocationObject | null>(null);
+
+  async function requestLocationPermission() {
+    // ask user for permission
+    const { granted } = await requestForegroundPermissionsAsync();
+
+    if (granted) {
+      const currentPosition = await getCurrentPositionAsync();
+
+      setLocation(currentPosition);
+    }
+  }
+
+  useEffect(() => {
+    requestLocationPermission();
+  }, []);
+
+  useEffect(() => {
+    // to observe the location
+    watchPositionAsync(
+      {
+        accuracy: LocationAccuracy.Highest,
+        timeInterval: 1000,
+        distanceInterval: 1,
+      },
+      (response) => {
+        console.log("nova localização", JSON.stringify(response, null, 2));
+        setLocation(response);
+      }
+    );
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {location ? (
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: location?.coords.latitude,
+            longitude: location?.coords.longitude,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: location?.coords.latitude,
+              longitude: location?.coords.longitude,
+            }}
+          />
+        </MapView>
+      ) : (
+        <Text style={styles.text}>Você ainda não tem uma localização</Text>
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
